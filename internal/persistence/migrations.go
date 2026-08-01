77ababb67437365a7a4be5023647a428c58747a1
Emil <1063362+asabla@users.noreply.github.com>
2026-08-01 15:07:38 +0200

    feat(authentication): advance DPoP replay schema

--- internal/persistence/migrations.go
+++ internal/persistence/migrations.go
@@ -13,7 +13,7 @@ import (
 )
 
 const (
-	currentSchemaVersion int64 = 15
+	currentSchemaVersion int64 = 16
 	migrationLockKey     int64 = 0x4441544147524f55
 	upMarker                   = "-- dataground:up"
 	downMarker                 = "-- dataground:down"
