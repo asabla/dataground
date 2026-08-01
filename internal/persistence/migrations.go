9c14a7f587d327b3f304349ccf65b0883777a3a5
Emil <1063362+asabla@users.noreply.github.com>
2026-08-01 14:36:52 +0200

    feat(authentication): audit authentication attempts

--- internal/persistence/migrations.go
+++ internal/persistence/migrations.go
@@ -13,7 +13,7 @@ import (
 )
 
 const (
-	currentSchemaVersion int64 = 14
+	currentSchemaVersion int64 = 15
 	migrationLockKey     int64 = 0x4441544147524f55
 	upMarker                   = "-- dataground:up"
 	downMarker                 = "-- dataground:down"
